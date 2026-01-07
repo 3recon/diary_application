12bdfccc6d2f854342db965cf13f215b3b4b8934
recon3 <jch200325@naver.com>
2026-01-07 13:50:32 +0900

    메인화면 구현만 완료

---
+++ app/src/main/java/com/example/one_line_memo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.one_line_memo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton btnMenu, imgbtnclose;
+    EditText edtMemo;
+    Button btnSaveMemo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.sidemenu);
+        btnMenu = findViewById(R.id.btnMenu);
+        imgbtnclose=findViewById(R.id.imgbtnclose);
+        btnSaveMemo=findViewById(R.id.btnSaveMemo);
+        edtMemo=findViewById(R.id.edtMemo);
+
+        btnMenu.setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.END);
+        });
+
+        imgbtnclose.setOnClickListener(view -> {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        });
+
+        btnSaveMemo.setOnClickListener(v->{
+            int memoLength=edtMemo.getText().toString().length();
+
+            if (memoLength>40) {
+                Toast.makeText(MainActivity.this,"40자 미만으로 작성해주세요",Toast.LENGTH_SHORT).show();
+            }
+            if (memoLength==0) {
+                Toast.makeText(MainActivity.this,"일기를 작성해주세요",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                //Todo: 로딩화면으로 이동(돌아가는 C)
+            }
+        });
+    }
+}
